7af054d13827f847935261ce5b7f7f38c526f1c6
Soh Thiam Hing <daffodilistic@gmail.com>
2018-08-30 12:15:00 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid h-100  al-s8-converter-bg" id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld/>
   </div>
 </template>
 
@@ -17,12 +17,22 @@ export default {
 </script>
 
 <style>
+.bg-fullscreen {
+  height: 100%;
+}
+
+.al-s8-converter-bg {
+  background: url(assets/Illo1.jpg) no-repeat;
+  background-position: center 30%;
+  background-attachment: fixed;
+  background-size: cover;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,184 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="row align-items-center">
+      <div class="container-fluid h-100">
+        <div class="row justify-content-center mt-4 mb-4">
+          <div class="col-12 col-lg-8">
+              <div class="bg-translucent text-white text-raleway pb-4">
+                <div class="row justify-content-center">
+                  <h1 class="header text-center pt-4 text-uppercase">Adventurers League<br>Season 8 Converter</h1>
+                </div>
+                <div class="row justify-content-center mt-4">
+                  <div class="col-10 col-md-5 text-center my-auto">
+                    <label class="" for="charXp">Character Experience Points</label>
+                  </div>
+                  <div class="col-10 col-md-5">
+                    <input v-model="charXP" type="number" class="h-100 form-control" id="charXp" placeholder="17000">
+                  </div>
+                </div>
+                <div class="row justify-content-center mt-4">
+                  <div class="col-10">
+                    <div class="row justify-content-center">
+                      <button v-on:click="getAP" type="button" data-toggle="modal" data-target="#convertModal" class="flex-grow-1 flex-md-grow-0 btn btn-danger text-uppercase"><h4 class="m-1">Calculate!</h4></button>
+                    </div>
+                  </div>
+                </div>
+                <div class="row justify-content-center mt-4">
+                  <span class="text-fine-print">Hand-crafted by <a href="https://www.twitter.com/daffodilistic" target="_blank">@daffodilistic</a></span>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" id="convertModal" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h3 class="text-raleway modal-title"><b>Conversion Results</b></h3>
+            <button type="button" class="close" data-dismiss="modal">
+              <span>&times;</span>
+            </button>
+          </div>
+          <div class="text-raleway modal-body">
+            <div class="text-center container">
+              Your character is now:
+              <h4 class="text-primary"><b>Level {{ charData.level }}</b></h4>
+              <h5><b>Current Advancement Checkpoints*</b></h5>
+              <div class="row justify-content-center text-success align-items-center">
+                <div class="col-5 text-center">Normal Advancement</div>
+                <div class="col-5"><b>{{ charData.advancementPoints.normal }}</b></div>
+              </div>
+              <div class="row justify-content-center text-danger align-items-center">
+                <div class="col-5 text-center">Slow Advancement</div>
+                <div class="col-5"><b>{{ charData.advancementPoints.slow }}</b></div>
+              </div>
+              <div class="row justify-content-center align-items-center mt-4">
+                <span class="text-fine-print">*NOTE:&nbsp;<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round" target="_blank">Default rounding</a>&nbsp;is used</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+  data: function() {
+    return {
+      charXP: "",
+      charData: {
+        level: 0,
+        advancementPoints: {}
+      }
+    };
+  },
+  methods: {
+    getAP: function() {
+      // TODO: Tweak/turn off eslint, this is getting ridiculous
+      var charExperience = isNaN(parseInt(this.charXP))
+        ? 0
+        : parseInt(this.charXP);
+
+      // From MPMB: https://github.com/morepurplemorebetter/MPMBs-Character-Record-Sheet/blob/master/_variables/Lists.js
+      var experiencePointsList = [
+        0,
+        300,
+        900,
+        2700,
+        6500,
+        14000,
+        23000,
+        34000,
+        48000,
+        64000,
+        85000,
+        100000,
+        120000,
+        140000,
+        165000,
+        195000,
+        225000,
+        265000,
+        305000,
+        355000
+      ];
+      //var levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+      var tiers = [
+        { levelLimit: 4, checkPoints: 4 },
+        { levelLimit: 10, checkPoints: 8 },
+        { levelLimit: 16, checkPoints: 8 },
+        { levelLimit: 20, checkPoints: 8 }
+      ];
+
+      var nextCharLevel = 0;
+      var experienceRemainder = 0;
+      var currentTier = 0;
+      experiencePointsList.forEach(amount => {
+        if (charExperience >= amount) {
+          nextCharLevel++;
+          experienceRemainder = charExperience - amount;
+        }
+
+        if (nextCharLevel + 1 > tiers[currentTier].levelLimit) {
+          currentTier++;
+        }
+      });
+
+      if (experienceRemainder >= experiencePointsList[19]) {
+        // Capped!
+      }
+
+      var shortfall =
+        experienceRemainder /
+        (experiencePointsList[nextCharLevel] -
+          experiencePointsList[nextCharLevel - 1]);
+      var advancementPoints = Math.round(
+        shortfall * tiers[currentTier].checkPoints
+      );
+
+      // console.log("Char level is " + nextCharLevel);
+      // console.log("Char Tier is " + (currentTier + 1));
+      // console.log("AP is " + advancementPoints);
+      // console.log("Next level XP is " + experiencePointsList[(nextCharLevel)]);
+      // console.log("Remainder is " + experienceRemainder);
+      // console.log("Shortfall % is " + shortfall);
+      // console.log(Utilities.formatString('Level %d, %f AP',nextCharLevel, advancementPoints));
+
+      var returnValue = {
+        level: nextCharLevel,
+        advancementPoints: {
+          normal: advancementPoints,
+          slow: advancementPoints / 2.0
+        }
+      };
+
+      this.charData = returnValue;
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.text-fine-print {
+  font-size: 0.75em;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.text-raleway {
+  font-family: "Raleway", sans-serif;
+  /* color: #fff; */
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.bg-translucent {
+  background: rgba(22, 22, 22, 0.8);
+  border-radius: 5px;
 }
-a {
-  color: #42b983;
+
+.header {
+  font-family: "Noto Sans", sans-serif;
 }
 </style>
